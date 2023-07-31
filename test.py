5559f4ac66bed2b152057299231f9229b2ff1856
Chu Qiao <qc1994qc099@gmail.com>
2023-07-31 10:22:36 -0400

    Update

--- test.py
+++ test.py
@@ -2,4 +2,6 @@ import os
 
 # test file
 print("hello world")
-print(os.environ["SECRET"])
+secret = os.environ["SECRET"]
+print(secret)
+print(f"This is not a secret {secret}, 123456789, 1|23456789")
